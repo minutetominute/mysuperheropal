e5af76980f365a6f173c3db23049e92c1a808be1
App Academy Student <appacademy@Apps-Mac-mini-39.local>
2015-05-12 11:06:09 -0400

    generate rails app, user model, users controller, and static pages controller

---
+++ app/assets/javascripts/mysuperheropal.js
@@ -0,0 +1,13 @@
+window.Mysuperheropal = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Mysuperheropal.initialize();
+});
---
+++ db/migrate/20150512143129_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.text :email, null: false
+      t.text :password_digest, null: false
+      t.text :session, null: false
+      t.text :superhero_name, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
