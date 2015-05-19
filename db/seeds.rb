2eede9b4c5902642f1dfd0d7063b90792afeab50
Jose Garza <minutetominute@gmail.com>
2015-05-19 18:06:25 -0400

    begin writing seeds scripts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,1 @@
 user = User.create!(email: "thehulk@gmail.com", password: "123456", superhero_name: "The Hulk")
-
-Food.create!(name: "uranium", calories: 0, radiation: "123", picture_url: "http://upload.wikimedia.org/wikipedia/commons/d/d4/Yellowcake.jpg")
-Food.create!(name: "banana", calories: 105, radiation: 1, picture_url: "http://upload.wikimedia.org/wikipedia/commons/1/1c/Bananas_white_background.jpg")
-Food.create!(name: "brazil nuts", calories: 186, radiation: 1, picture_url: "https://nuts.com/images/auto/801x534/assets/e8c4042b13505436.jpg")
-
-Exercise.create!(name: "car press", calories_burned: 1000)
-Exercise.create!(name: "global lap", calories_burned: "1000000")
-Exercise.create!(name: "atlantic swim", calories_burned: "10000000")
---
+++ lib/scripts/foods_seed_generator.rb
@@ -0,0 +1,9 @@
+require 'rest-client'
+
+current_api_key_requests = 0;
+successful_requests = 0;
+total_entries = 8617;
+
+until successful_requests = total_entries
+	
+end
